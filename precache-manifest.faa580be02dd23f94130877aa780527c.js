91e342afc3202ffaa6a1f4627e6b016e63bbff19
Sandeep <ssabat35@gmail.com>
2020-09-29 10:58:36 +0530

    Updates

---
+++ precache-manifest.faa580be02dd23f94130877aa780527c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce1d2deb65a0c500128e5f2984e17de1",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "7c3dbffbeb571e9c3496",
+    "url": "/calculator/static/css/main.7da09109.chunk.css"
+  },
+  {
+    "revision": "799fbf9a45739ffa1c19",
+    "url": "/calculator/static/js/2.4441aaf4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.4441aaf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c3dbffbeb571e9c3496",
+    "url": "/calculator/static/js/main.951439dd.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
